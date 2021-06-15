61f9f1d7ee35268382f394eccc7bfdcdc9d1ca40
deadromeo <barkovskiis@mail.ru>
2021-06-15 21:38:48 +0300

    first commit

---
+++ ext.php
@@ -0,0 +1,18 @@
+<?php
+/**
+*
+* @package Snow
+* @copyright (c) 2020 DeaDRoMeO ; hello-vitebsk.ru
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace deadromeo\snow;
+
+/**
+* @ignore
+*/
+
+class ext extends \phpbb\extension\base
+{
+}
